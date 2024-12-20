3e167b1f9d3e9e4194b74c510a5393d6b20d1e05
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2024-12-20 13:19:32 +0545

    React new repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Content from "./content"
+import Footer from "./Footer"
+import Header from "./header"
+
+function App(){
+  return(
+    <>
+    <Header></Header>
+    <Content></Content>
+    <Footer></Footer>
+    
+    </>
+  )
+}
+
+export default App
