b6291e7967b2e915c5bcc8766f305af9af359863
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2024-12-20 14:09:07 +0545

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Content from "./content"
 import Footer from "./Footer"
 import Header from "./header"
+import "./App.css"
 
 function App(){
   return(
     <>
-    <Header></Header>
-    <Content></Content>
-    <Footer></Footer>
+    <Header/>
+    <Content/>
+    <Footer/>
     
     </>
   )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,20 @@
+import "./Header.css"
 function Header() {
   return (
-    <div>
-        
-    </div>
-  )
+    <>
+      <div className="main_division_of_header">
+        <div>
+          <img src="/images/logo.svg" alt="imae"    className="image_of_logo" />
+        </div>
+        <nav className="navigation">
+          <a className="navigation_links">Home</a>
+          <a className="navigation_links">Gallery</a>
+          <a className="navigation_links">Contact</a>
+          <a className="navigation_links">About</a>
+        </nav>
+      </div>
+    </>
+  );
 }
 
-
-export default Header
+export default Header;
