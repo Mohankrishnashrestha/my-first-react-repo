0147796ea9c26e6bf626994dac421822d4e68ff4
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2024-12-21 21:28:44 +0545

    added

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,84 @@
+import "./Content.css";
 function Content() {
   return (
-    <div>Content</div>
-  )
+    <>
+      <div>
+        <div class="div-of-main-1">
+          <section class="images-of-arthik">
+            <img
+              src="/images/group photo.jpg"
+              alt="group image"
+              class="img-group"
+            />
+          </section>
+
+          <section class="introduction-of-arthik">
+            <div>
+              <h2 class="introduction">INTRODUCTION</h2>
+            </div>
+            <div class="div-of-introduction">
+              <p class="paragraph-of-introduction">
+                " The Aarthik Bikash Mitra Saving and Credit Cooperative Society
+                Ltd. was established on 5th Poush 2061 BS (December 2004 AD) by
+                37 members, including 28 men and 9 women, with the intention of
+                serving the then VDCs (Village Development Committees) of Hokse,
+                Anikot, Mahadevsthan, Dolalghat, and Bhumlu in the
+                Kavrepalanchok district. The cooperative aimed to provide
+                services desired by its members, collect scattered... "
+              </p>
+            </div>
+          </section>
+        </div>
+        <div class="message-and-instruction-cycle">
+          <div class="message-and-image">
+            <section class="message-part">
+              <div>
+                <h3 class="message">Message</h3>
+              </div>
+              <div class="div-of-figure">
+                <img
+                  src="/images/figure.jpeg"
+                  alt="figure"
+                  class="images-of-figure"
+                />
+              </div>
+              <div>
+                <span class="name-of-the-oficer">Thakurnath Adhikari</span>
+                <span class="name-of-the-oficer">Chief Executive Officer</span>
+              </div>
+            </section>
+            <section class="description-of-the-officer">
+              <p class="paragraph-of-description">
+                " Since the establishment of the institution, technology has
+                made significant advances, and today, every activity of the
+                institution is managed in a way that allows members to view and
+                study them at their convenience.... "
+              </p>
+            </section>
+          </div>
+          <div class="div-instruction">
+            <section>
+              <h4 class="heading-of-instruction-cycle">INCTRUCTOIN CYCLE</h4>
+            </section>
+            <section class="applicaion-of-instruction-cycle">
+              <div class="dic-ic">
+                <div class="ic">share capital</div>
+                <div class="ic">loan invesment</div>
+                <div class="ic">total aspect</div>
+                <div class="ic">share members</div>
+              </div>
+              <div class="dic-ic">
+                <div class="ic">saving deposits</div>
+                <div class="ic">profit loss</div>
+                <div class="ic">liquidity</div>
+                <div class="ic">bal member</div>
+              </div>
+            </section>
+          </div>
+        </div>
+        </div>
+    </>
+  );
 }
 
-export default Content
+export default Content;
